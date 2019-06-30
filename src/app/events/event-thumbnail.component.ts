83f2ba9b7a362f2d463c536972c13cb2424658b5
idvillota <idvillota@gmail.com>
2019-06-30 12:55:53 -0500

    Add files via upload

---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,51 @@
+import { Component, Input, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'event-thumbnail',
+    template:`
+        <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
+            <h2>{{event?.name}}</h2>
+            <div>Date: {{event?.date}}</div>
+            <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+                Time: {{event?.time}}
+                <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                <span *ngSwitchDefault>(Normal Start)</span>
+            </div>
+            <div>Price: \${{event?.price}}</div>
+            <div [hidden]="!event?.location">
+                <span>Location: {{event?.location?.address}}</span>
+                <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+            </div>
+            <div *ngIf="event?.onlineUrl">
+                Online URL: {{event?.onlineUrl}}
+            </div>
+
+            <button class="btn btn-primary" (click)="handleClickMe()"> Click me! </button>            
+        </div>            
+    `,
+    styles:[`
+            .green { color: #003300 !important; }
+            .bold { font-weight: bold; }
+            .thumbnail { min-height: 210px; }
+            .pad-left { margin-left: 10px; }
+            .well div { color: #bbb;}
+        `
+    ]
+})
+
+export class EventThumbnailComponent{
+    @Input() event:any;
+    @Output() eventClick = new EventEmitter();
+
+    handleClickMe(){
+        this.eventClick.emit(this.event.name);
+    }
+
+    getStartTimeClass(){
+        if(this.event && this.event.time === '8:00 am')
+            return ['green', 'bold']
+        return []
+    }
+}
---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core'
+import { EventService } from './shared/event.service';
+import { ToastrService } from '../common/toastr.service'
+
+@Component({
+    template: `
+        <div>
+            <h1>Upcoming Angular Events</h1>
+            <hr/>   
+            <div class="row">
+                <div *ngFor="let event of events" class="col-md-5">
+                    <event-thumbnail #thumbnail (eventClick)="handleEventClicked($event)" (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>  
+                </div>
+            </div>
+        </div>
+    `
+})
+
+export class EventsListComponent implements OnInit {
+    
+  events: any[]
+  
+    constructor(private eventService: EventService, private toastr:ToastrService){
+    }
+
+    ngOnInit(){
+      this.events = this.eventService.getEvents()
+    }
+
+    handleEventClicked(data){
+        console.log('Received: ', data)
+    }
+
+    handleThumbnailClick(eventName){
+        this.toastr.success(eventName)
+    }
+
+}

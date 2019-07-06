2e7436e558abe374f4af42880eb07a750daf9d01
Ivan Villota <idvillota@gmail.com>
2019-07-05 19:00:36 -0500

    Finish chapter 12 regarding Creating directives

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../user/auth.service';
+import { ISession, EventService } from '../events';
 
 @Component({
     selector: 'nav-bar',
@@ -13,9 +14,18 @@ import { AuthService } from '../user/auth.service';
 })
 
 export class NavBarComponent{
+    searchTerm: string = ""
+    foundSessions: ISession[]
 
-    constructor(public auth:AuthService){
-        
+    constructor(public auth:AuthService, private eventService:EventService){        
+    }
+
+    searchSessions(searchTerm){
+        this.eventService.searchSessions(searchTerm).subscribe
+            (sessions => {
+                this.foundSessions = sessions
+                console.log(this.foundSessions)
+            })
     }
 
 }
